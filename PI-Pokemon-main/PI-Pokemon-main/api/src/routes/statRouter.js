4ca4a234b2c7a632fb353936f3e50ce80a3eaad8
Matias Barrotto <94069375+MatiasBarrotto@users.noreply.github.com>
2022-11-18 20:35:44 -0300

    Delete PI-Pokemon-main/PI-Pokemon-main directory

--- PI-Pokemon-main/PI-Pokemon-main/api/src/routes/statRouter.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-
-const statRouter = Router();
-
-statRouter.get("/", (req,  res) =>{
-    res.send("Estos son los stats");
-});
-
-module.exports = statRouter;
